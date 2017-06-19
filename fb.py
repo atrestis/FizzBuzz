acbced429b1b53bf9f7d0bee60570a6142b50e6a
Atrestis Karalis <atrestis@gmail.com>
2017-06-19 16:55:04 +0300

    new files & minor fixes

--- fb.py
+++ fb.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import time
 class FizzBuzz:
 
 	def myawesomescript(self):
@@ -7,17 +8,22 @@ class FizzBuzz:
 			if (x%3==0):
 				if ((x%3==0) & (x%5==0)):
 					print("FizzBuzz")
+					time.sleep(1)
 				else:
 					print("Fizz")
+					time.sleep(1)
 			elif (x%5==0):
 				if ((x%3==0) & (x%5==0)):
 					print("FizzBuzz")
 				else:
 					print("Buzz")
+					time.sleep(1)
 			else:
 				print(x)
+				time.sleep(1)
 
 if __name__ == '__main__':
 	print("Running as a script...")
+	time.sleep(0.5)
 	fb = FizzBuzz()
 	fb.myawesomescript()
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='FizzBuzz',
+      version='1.0',
+      description='Practicing game',
+      author='Atrestis Karalis',
+      author_email='atrestis@gmail.com',
+      py_modules=['fb'],
+     )
